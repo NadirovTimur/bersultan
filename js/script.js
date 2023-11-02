629faf0dce9b3fe5a11978f9833c4acc60bc2ceb
Timur_Nadirov <nadirov_timur@mail.ru>
2023-11-02 21:49:22 +0700

    home7

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ window.onclick = function (event) {
 var modal_diagnostic = document.getElementById('modal_diagnostic');
 
 
-var diagnostic = document.getElementById("diagnostic");
+var diagnostic = document.getElementsByClassName("diagnostic");
 
 
 var span1 = document.getElementsByClassName("close")[1];
@@ -48,3 +48,29 @@ window.onclick = function (event) {
         modal_diagnostic.style.display = "none";
     }
 }
+
+var wheels = document.getElementById("wheels");
+
+
+
+wheels.onclick = function () {
+    modal_diagnostic.style.display = "block";
+}
+
+engines.onclick = function () {
+    modal_diagnostic.style.display = "block";
+}
+
+var modal_diagnostic1 = document.getElementById('modal_diagnostic1');
+var wheels1 = document.getElementById("wheels1");
+
+wheels1.onclick = function () {
+    modal_diagnostic1.style.display = "block";
+}
+
+engines1.onclick = function () {
+    modal_diagnostic1.style.display = "block";
+}
+
+
+
