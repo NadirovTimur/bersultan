4f87a8faed0d829e8adb6bbc3318466896a4ca09
Beksultan Almazbekov <anime.beksultan@gmail.com>
2023-11-02 12:14:50 +0600

    home3

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-var modal = document.getElementById('myModal');
+var modal = document.getElementById('modal1');
 
 
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("show1");
 
 
 var span = document.getElementsByClassName("close")[0];
@@ -20,5 +20,31 @@ span.onclick = function () {
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    }
+}
+
+
+var modal_diagnostic = document.getElementById('modal_diagnostic');
+
+
+var diagnostic = document.getElementById("diagnostic");
+
+
+var span1 = document.getElementsByClassName("close")[1];
+
+
+diagnostic.onclick = function () {
+    modal_diagnostic.style.display = "block";
+}
+
+
+span1.onclick = function () {
+    modal_diagnostic.style.display = "none";
+}
+
+
+window.onclick = function (event) {
+    if (event.target == modal_diagnostic) {
+        modal_diagnostic.style.display = "none";
     }
 }
